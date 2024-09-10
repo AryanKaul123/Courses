befab85048b27feac884a94f49dfeaa5747cf355
AryanKaul123 <160542508+AryanKaul123@users.noreply.github.com>
2024-09-10 13:59:28 +0530

    first commit

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,27 +1,14 @@
 import Card from "./Card";
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Cards = (props) => {
     console.log(props.category);
     console.log(props.courses);
 
-    let category = props.category;
     const [likedCourses, setLikedCourses] = useState([]);
-    //   let allCourse = [];
-
-    // It Returns list of all courses received from the api Response
-
-    // const getCourses = () => {
-    //     Object.values(props.courses).forEach((courseCategory) => {
-    //         courseCategory.forEach((course) => {
-    //             allCourse.push(course);
-    //         });
-    //     });
-    //     return allCourse;
-    // };
 
     function getCourses() {
-        if (category === "All") {
+        if (props.category === "All") {
             let allCourses = [];
             Object.values(props.courses).forEach((array) => {
                 array.forEach((courseData) => {
@@ -29,21 +16,21 @@ const Cards = (props) => {
                 });
             });
             return allCourses;
-        }
-        else
-        {
-            return props.courses[category];
+        } else {
+            return props.courses[props.category] || [];
         }
     }
 
-    //   console.log(allCourse);
     return (
         <div className="flex flex-wrap justify-center gap-4 mb-4">
-            {
-                getCourses().map((course) => {
-                    return <Card course={course} key={props.courses.id} likedCourses={likedCourses} setLikedCourses={setLikedCourses} />;
-                })
-            }
+            {getCourses().map((course) => (
+                <Card 
+                    course={course} 
+                    key={course.id}  // Use a unique identifier from the course object
+                    likedCourses={likedCourses} 
+                    setLikedCourses={setLikedCourses} 
+                />
+            ))}
         </div>
     );
 };
